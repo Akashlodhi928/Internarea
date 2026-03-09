1334914aaef6ce19fe384eff743e770620b1700d
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2026-03-09 23:01:32 +0530

    Update index.tsx

--- internarea/src/pages/index.tsx
+++ internarea/src/pages/index.tsx
@@ -120,8 +120,8 @@ export default function SvgSlider() {
     const fetchdata = async () => {
       try {
         const [internshipres, jobres] = await Promise.all([
-          axios.get("http://localhost:5000/api/internship"),
-          axios.get("http://localhost:5000/api/job"),
+          axios.get("https://internarea-jxck.onrender.com/api/internship"),
+          axios.get("https://internarea-jxck.onrender.com/api/job"),
         ]);
         setinternship(internshipres.data);
         setjob(jobres.data);
@@ -132,8 +132,6 @@ export default function SvgSlider() {
     fetchdata();
   }, []);
 
-  // http://localhost:5000
-  // http://localhost:5000
   
   const [selectedCategory, setSelectedCategory] = useState("");
   const filteredInternships = internships.filter(
