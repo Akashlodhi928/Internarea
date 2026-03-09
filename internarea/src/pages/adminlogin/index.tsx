da98349a7495fc24351752f00acae885a2fb9a38
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2026-03-09 22:54:26 +0530

    Update index.tsx

--- internarea/src/pages/adminlogin/index.tsx
+++ internarea/src/pages/adminlogin/index.tsx
@@ -28,7 +28,7 @@ const index = () => {
     try {
       setisloading(true);
       const res = await axios.post(
-        "http://localhost:5000/api/admin/adminlogin",
+        "https://internarea-jxck.onrender.com/api/admin/adminlogin",
         formadata
       );
       toast.success("logged in successfuly");
